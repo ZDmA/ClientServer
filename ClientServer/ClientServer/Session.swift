03e90688186984eccdfef818a7085296b8971726
Dmitriy <main-zd@mail.ru>
2022-08-06 23:38:50 +0300

    Исходный проект

---
+++ ClientServer/ClientServer/Session.swift
@@ -0,0 +1,8 @@
+//
+//  Session.swift
+//  ClientServer
+//
+//  Created by Дмитрий on 03.08.2022.
+//
+
+import Foundation
---
+++ ClientServer/ClientServer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientServer
+//
+//  Created by Дмитрий on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
